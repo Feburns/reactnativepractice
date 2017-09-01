17b2adce921341fdb5536ee7c8bca4f57cea958f
Parkinson, Christopher (Technology Graduate) <christopher.parkinson@sky.uk>
2017-09-01 16:31:06 +0100

    Text position and sizing changes

--- src/containers/MemeScreen3.js
+++ src/containers/MemeScreen3.js
@@ -37,9 +37,9 @@ export class MemeScreen3 extends React.Component {
         style={{width: 320, height: 320}}
         source={require('../../img/meme3.jpg')}>
         <View style={{paddingTop: 60, width: 320, height: 120}}>
-          <Text style={ {fontSize: 20, fontFamily:'sans-serif-medium' , textAlign: 'left', backgroundColor: 'rgba(0,0,0,0)', color: 'white', position: "absolute", left:10, top:10, width:290}}
+          <Text style={ {fontSize: 17, fontFamily:'sans-serif-medium' , textAlign: 'left', backgroundColor: 'rgba(0,0,0,0)', color: 'white', position: "absolute", left:10, top:5, width:120}}
           > {navigate.state.params.input}</Text>
-          <Text style={ {fontSize: 20, fontFamily:'sans-serif-medium' , textAlign: 'left', backgroundColor: 'rgba(0,0,0,0)', color: 'white', position: "absolute", top:10 ,left:190, width:290}}
+          <Text style={ {fontSize: 17, fontFamily:'sans-serif-medium' , textAlign: 'left', backgroundColor: 'rgba(0,0,0,0)', color: 'white', position: "absolute", top:5 ,left:190, width:90}}
           > {navigate.state.params.input2} </Text>
         </View>
         </Image>
